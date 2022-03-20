8deb88401efa99cb454d67d01d80fc6bbb04bde5
parkchu <parkjoohan0603@gmail.com>
2022-03-21 04:23:58 +0900

    add more cars and change graphic

--- racing-car/main.c
+++ racing-car/main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 typedef struct _Car {
 	int number;
 	int moveTime;
 } Car;
+
+void getInputValue(int *carsNumber, int *times)
+{
+	printf("몇개의 자동차로 경주를 할건가요? : ");
+        scanf("%d", carsNumber);
+        printf("시도횟수는 몇회 인가요? : ");
+        scanf("%d", times);
+        printf("------------------------------------ \n");
+}
 
 void makeCars(int carsNumber, Car *cars)
 {
@@ -32,24 +42,35 @@ void go(Car *car, int randomValue)
 void main()
 {
 	int carsNumber;
-	int i;
+	int times;
+	int index;
 	int randomValue;
 	Car *cars;
 
-	printf("몇개의 자동차로 경주를 할건가요? : ");
-	scanf("%d", &carsNumber);
+	getInputValue(&carsNumber, &times);
+
 	cars = malloc(sizeof(Car) * carsNumber);
 	makeCars(carsNumber, cars);
+
 	srand(time(NULL));
-	i = 0;
-	while (i < carsNumber)
+	char *test = malloc(sizeof(char) * times);
+	index = 0;
+	while (times > 0)
 	{
-		Car car = cars[i];
+		memset(test, 0, times);
+		Car *car = cars + index;
 		randomValue = rand() % 10 + 1;
-		go(&car, randomValue);
-		printf("%d : %d \n", car.number, car.moveTime);
-		i++;
+		go(car, randomValue);
+		memset(test, '-', car->moveTime);
+		printf("%d : %s : %d \n", car->number, test, car->moveTime);
+		index++;
+		if (index == carsNumber)
+		{
+			printf("------------------------------------ \n");
+			index = 0;
+			times--;
+		}
 	}
-	printf("------------------------------------ \n");
 	free(cars);
+	free(test);
 }
