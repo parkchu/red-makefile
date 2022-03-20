b1d8a15f097691e923a583342b71a4692376219a
parkchu <parkjoohan0603@gmail.com>
2022-03-20 19:27:44 +0900

    move a car when randomValue is bigger or equal than as 4

--- racing-car/main.c
+++ racing-car/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct _Car {
 	int number;
+	int moveTime;
 } Car;
 
 void makeCars(int carsNumber, Car *cars)
@@ -13,26 +15,41 @@ void makeCars(int carsNumber, Car *cars)
 	{
 		Car car;
 		car.number = number;
+		car.moveTime = 0;
 		cars[number - 1] = car;
 		number++;
+	}
+}
+
+void go(Car *car, int randomValue)
+{
+	if (randomValue >= 4)
+	{
+		car->moveTime++;
 	}
 }
 
 void main()
 {
 	int carsNumber;
-	int time;
+	int i;
+	int randomValue;
 	Car *cars;
 
 	printf("몇개의 자동차로 경주를 할건가요? : ");
 	scanf("%d", &carsNumber);
 	cars = malloc(sizeof(Car) * carsNumber);
 	makeCars(carsNumber, cars);
-	time = 0;
-	while (time < carsNumber)
+	srand(time(NULL));
+	i = 0;
+	while (i < carsNumber)
 	{
-		printf("%d \n", cars[time].number);
-		time++;
+		Car car = cars[i];
+		randomValue = rand() % 10 + 1;
+		go(&car, randomValue);
+		printf("%d : %d \n", car.number, car.moveTime);
+		i++;
 	}
+	printf("------------------------------------ \n");
 	free(cars);
 }
