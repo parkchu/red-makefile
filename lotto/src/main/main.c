4e9da2da2be6df14fc6ef8a743c1a5fb9e2a6435
parkchu <parkjoohan0603@gmail.com>
2022-04-05 02:18:31 +0900

    set up TDD environment

---
+++ lotto/src/main/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "test.h"
+
+void main()
+{
+	printf("%d", sum(10, 20));
+	printf("\n 응애애애ㅐ");
+}
---
+++ lotto/src/test/testTest.c
@@ -0,0 +1,18 @@
+#include "unity/src/unity.h"
+#include "../main/test.h"
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+void test_sum(void) {
+	TEST_ASSERT_EQUAL(1, sum(3, 7));
+}
+
+int main (void) {
+	UNITY_BEGIN();
+	RUN_TEST(test_sum);
+	UNITY_END();
+}
