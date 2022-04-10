d340a9269b3d39cdc79168fa671aec8209200530
parkchu <parkjoohan0603@gmail.com>
2022-04-10 16:17:39 +0900

    delete unncessary files

--- lotto/src/main/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "test.h"
-
-int main(void)
-{
-	printf("%d \n", sum(10, 10));
-	printf("I am red \n");
-}
--- lotto/src/study/study2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("나는 study2 임돠");
-}
